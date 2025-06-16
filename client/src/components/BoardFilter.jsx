ec9f75ec250923617ecdd70cbb425652f1479e40
Ana Ramirez <anagramirez@meta.com>
2025-06-16 14:07:10 -0700

    Implement category filter

---
+++ client/src/components/BoardFilter.jsx
@@ -0,0 +1,26 @@
+
+import React from "react";
+import "./BoardFilter.css";
+
+const categories = ["All", "Recent", "Celebration", "Thank You", "Inspiration"];
+
+const BoardFilter = ({selectedCategory, onSelect}) => {
+  return (
+    <div className="board-filter">
+      <div className="board-filter-scroll">
+        {categories.map((category) => (
+        <button
+          key={category}
+          className={`filter ${selectedCategory === category ? "active" : ""}`}
+          onClick={() => onselect(category)}
+        >
+          {category}
+        </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default BoardFilter;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import BoardFilter from "../components/BoardFilter"
 
 const Home = () => {
   return (
     <div>
+
       <Header></Header>
+      <BoardFilter></BoardFilter>
+      
     </div>
   );
 };
