89beb4481454d7f07d08e08e1f734e2172e31a1d
Ana Ramirez <anagramirez@meta.com>
2025-06-16 12:48:03 -0700

    implement a Header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <img className="logo" src="/logo.png" alt="kudos board logo" />
+      </div>
+      <div classNmae="header-text">
+        <h1 className="title"> KUDOSBOARD</h1>
+        <h3 className="slogan">insert funny and cheesy phrase </h3>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
