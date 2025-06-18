7983de59fe729c5ab7757baba0ba3062f169427b
Ana Ramirez <anagramirez@meta.com>
2025-06-18 14:45:32 -0700

    integrate get all boards endpoint with frontend

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+const cors = require('cors')
 
+app.use(cors())
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
